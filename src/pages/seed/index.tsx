a2078996069f2b0ffbdf9749f96199bed5feca6e
Robson <robson.miranda@lunes.io>
2022-03-22 17:14:08 -0300

    added: validate seed

--- src/pages/seed/index.tsx
+++ src/pages/seed/index.tsx
@@ -1,63 +1,82 @@
-import { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { encryptAes } from "../../services/cryptograpy";
+import { useState } from "react"
+import { useNavigate, useLocation } from "react-router-dom"
+import { encryptAes } from "../../services/cryptograpy"
 
-import { ButtonNeutral, ButtonConfirm } from "../../components/button/index";
-import { ModalAlert } from "../../components/modal/index";
+import { ButtonNeutral, ButtonConfirm } from "../../components/button/index"
+import { ModalAlert } from "../../components/modal/index"
 
-import { decodeWallet, newSeed } from "../../services/lunes";
+import { decodeWallet, newSeed, validateSeed } from "../../services/lunes"
 
-import * as S from "./styles";
+import * as S from "./styles"
 
 const Seed = () => {
-  const navigate = useNavigate();
-  const location: any = useLocation();
-  const { password } = location.state;
-  const [modal, setModal] = useState("");
-  const [seed, setSeed] = useState("");
+    const navigate = useNavigate()
+    const location: any = useLocation()
+    const { password } = location.state
+    const [modal, setModal] = useState("")
+    const [seed, setSeed] = useState("")
 
-  return (
-    <S.Container>
-      <S.InputSeedTitle>Digite sua seed</S.InputSeedTitle>
-      <S.InputSeed
-        value={seed}
-        onChange={(event) => setSeed(event.target.value)}
-      />
+    return (
+        <S.Container>
+            <S.InputSeedTitle>Digite sua seed</S.InputSeedTitle>
+            <S.InputSeed
+                rows={5}
+                value={seed}
+                onChange={(event) => setSeed(event.target.value)}
+            />
 
-      <S.ButtonHolder>
-        <ButtonNeutral
-          action={() => {
-            const generated = newSeed();
-            setSeed(generated.phrase);
-          }}
-          label="Gerar nova"
-        />
-        <ButtonConfirm
-          action={() => {
-            if (!seed) {
-              return;
-            }
-            localStorage.setItem("SEED", encryptAes(seed, password));
-            localStorage.setItem("ADDRESS", decodeWallet(seed).address);
-            setModal("confirm");
-          }}
-          label="Validar"
-        />
-      </S.ButtonHolder>
+            <S.ButtonHolder>
+                <ButtonNeutral
+                    action={() => {
+                        const generated = newSeed()
+                        setSeed(generated.phrase)
+                    }}
+                    label="Gerar nova"
+                />
+                <ButtonConfirm
+                    action={() => {
+                        if (!validateSeed(seed.trim())) {
+                            setModal("seed-invalid")
+                            return
+                        }
+                        localStorage.setItem(
+                            "SEED",
+                            encryptAes(seed.trim(), password)
+                        )
+                        localStorage.setItem(
+                            "ADDRESS",
+                            decodeWallet(seed.trim()).address
+                        )
+                        setModal("confirm")
+                    }}
+                    label="Validar"
+                />
+            </S.ButtonHolder>
 
-      {modal === "confirm" && (
-        <ModalAlert
-          text="Sua seed foi adicionada com sucesso!"
-          type="success"
-          buttonLabel="Continuar"
-          onClose={() => {
-            setModal("");
-            navigate("/");
-          }}
-        />
-      )}
-    </S.Container>
-  );
-};
+            {modal === "confirm" && (
+                <ModalAlert
+                    text="Sua seed foi adicionada com sucesso!"
+                    type="success"
+                    buttonLabel="Continuar"
+                    onClose={() => {
+                        setModal("")
+                        navigate("/")
+                    }}
+                />
+            )}
 
-export default Seed;
+            {modal === "seed-invalid" && (
+                <ModalAlert
+                    text="A seed digitada é inválida"
+                    type="error"
+                    buttonLabel="Tentar de novo"
+                    onClose={() => {
+                        setModal("")
+                    }}
+                />
+            )}
+        </S.Container>
+    )
+}
+
+export default Seed
--- src/pages/seed/styles.ts
+++ src/pages/seed/styles.ts
@@ -1,44 +1,44 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Container = styled.div`
-  width: 100%;
-  overflow-y: scroll;
-  height: calc(100% - 58px);
-  padding: 20px;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
+    width: 100%;
+    overflow-y: scroll;
+    height: calc(100% - 58px);
+    padding: 20px;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`
 
 export const InputSeedTitle = styled.h1`
-  font-size: 1rem;
-  text-align: center;
-  margin-top: 20px;
-`;
+    font-size: 1rem;
+    text-align: center;
+    margin-top: 20px;
+`
 
 export const InputSeed = styled.textarea`
-  width: 100%;
-  height: 150px;
-  border: solid 1px ${({ theme }) => theme.colors.primary[400]};
-  resize: none;
-  border-radius: 8px;
-  background-color: transparent;
-  outline: none;
-  color: ${({ theme }) => theme.colors.themeColors[100]};
-  padding: 20px;
-  text-align: center;
-  vertical-align: middle;
-  margin-top: 15px;
-  font-family: "Noto Sans";
-`;
+    width: 100%;
+    border: solid 1px ${({ theme }) => theme.colors.primary[400]};
+    resize: none;
+    border-radius: 8px;
+    background-color: transparent;
+    outline: none;
+    color: ${({ theme }) => theme.colors.themeColors[100]};
+    padding: 20px;
+    text-align: center;
+    vertical-align: middle;
+    margin-top: 15px;
+    font-family: "Noto Sans";
+    font-size: 1.3rem;
+`
 
 export const ButtonHolder = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  margin-top: 12px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 12px;
 
-  button {
-    margin: 0 5px;
-  }
-`;
+    button {
+        margin: 0 5px;
+    }
+`
