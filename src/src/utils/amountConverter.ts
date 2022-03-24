9f06c381936466e8947eb13cf9cc2acb7b0e4948
Robson <robson.miranda@lunes.io>
2022-03-24 17:45:00 -0300

    fix: removed duplicated src folder

--- src/src/utils/amountConverter.ts
+++
@@ -1,5 +0,0 @@
-export const toSmallerCoinUnit = (amount: number, decimals: number) =>
-    amount * 10 ** decimals
-
-export const toBiggestCoinUnit = (amount: number, decimals: number) =>
-    amount / 10 ** decimals
