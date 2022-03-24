8c5b4664bd33e9ba669e5fc68d8b98235ec1b137
Robson <robson.miranda@lunes.io>
2022-03-24 10:03:14 -0300

    fix: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,24 @@
 import React from "react"
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 
 // Routes
 import Home from "../pages/home"
 import Seed from "../pages/seed"
 import Password from "../pages/password"
 import Advice from "../pages/advice"
+import Welcome from "../pages/welcome"
 
 export default function Router() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="/seed" element={<Seed />} />
                 <Route path="/pass" element={<Password />} />
                 <Route path="/advice" element={<Advice />} />
+                <Route path="/welcome" element={<Welcome />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
