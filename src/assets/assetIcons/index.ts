8ce52e50a0bc732678289c6b7c7a938fdc491c04
Robson <robson.miranda@lunes.io>
2022-03-30 23:56:48 -0300

    added: asset icons

---
+++ src/assets/assetIcons/index.ts
@@ -0,0 +1,11 @@
+import assetDefault from "./asset-default.png"
+import gold_mining from "./gold-mining.png"
+import lbrl from "./lbrl.png"
+import up_coin from "./up-coin.png"
+
+export const icons = {
+    default: assetDefault,
+    gold_mining: gold_mining,
+    lbrl: lbrl,
+    up_coin: up_coin
+}
