cd2032ec1fe8dcd6fe88ada86c9565fc2fd8ad96
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2022-04-01 13:54:55 -0300

    update: name icon

--- src/assets/assetIcons/index.ts
+++ src/assets/assetIcons/index.ts
@@ -1,11 +1,11 @@
-import lunes_developer_score from "./lunes-developer-score.png"
 import assetDefault from "./asset-default.png"
+import lunes_badge from "./lunes_badge.png"
 import gold_mining from "./gold-mining.png"
 import up_coin from "./up-coin.png"
 import lbrl from "./lbrl.png"
 
 export const icons = {
-    lunes_developer_score: lunes_developer_score,
+    lunes_badge: lunes_badge,
     gold_mining: gold_mining,
     default: assetDefault,
     up_coin: up_coin,
