72b38ad9a8a4f271f848e8949781d0765013c5a5
Robson <robson.miranda@lunes.io>
2022-05-02 14:39:59 -0300

    add: modal core

---
+++ src/modal/core/modal.tsx
@@ -0,0 +1,39 @@
+import { render } from "react-dom";
+import ModalContainer from "../components/modalContainer"
+
+import {
+    IModalAlert,
+    IModalConfirm,
+    IModalPassword,
+    IModalPrompt,
+} from "../types"
+
+import "../index.css"
+
+let containerDomNode: HTMLElement;
+
+function dispatchModal(props: any) {
+    console.log(props)
+    containerDomNode = document.createElement('div');
+    document.body.appendChild(containerDomNode);
+
+    render(<ModalContainer {...props} />, containerDomNode);
+}
+
+function modalAlert(props: IModalAlert) {
+    return dispatchModal({ ...props, appearance: "alert" })
+}
+
+function modalConfirm(props: IModalConfirm) {
+    return dispatchModal({ ...props, appearance: "confirm" })
+}
+
+function modalPrompt(props: IModalPrompt) {
+    dispatchModal({ ...props, appearance: "prompt" })
+}
+
+function modalPassword(props: IModalPassword) {
+    return dispatchModal({ ...props, appearance: "password" })
+}
+
+export { modalAlert, modalConfirm, modalPrompt, modalPassword }
