e7adc15bbc5f935d3ca5f4d8bfd9ff6b20f8cc1d
Robson <robson.miranda@lunes.io>
2022-05-04 12:29:19 -0300

    add: route to seed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,19 @@ import { HashRouter, Route, Routes } from "react-router-dom"
 // Routes
 import Home from "../pages/home"
 import Password from "../pages/password"
+import Seed from "../pages/seed"
 import Welcome from "../pages/welcome"
+import Terms from "../pages/terms"
 
 export default function Router() {
     return (
         <HashRouter>
             <Routes>
                 <Route element={<Home />} />
-                <Route path="/" element={<Password />} />
-                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/password" element={<Password />} />
+                <Route index element={<Welcome />} />
+                <Route path="/seed" element={<Seed />} />
+                <Route path="/terms" element={<Terms />} />
             </Routes>
         </HashRouter>
     )
