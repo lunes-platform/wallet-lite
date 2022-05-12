da68ae384b3279ad2dfd236ce8d107e6d1959ab7
Robson <robson.miranda@lunes.io>
2022-05-12 12:52:49 -0300

    removed warning

--- src/services/cotation.ts
+++ src/services/cotation.ts
@@ -1,18 +1,10 @@
-import axios from "axios"
-
+/**
+ *
+ * Insert an API call to get cotation from your provider
+ *
+ */
 export const getCotationFromApi = async (
     coin: string
 ): Promise<number | null> => {
     return null
-    try {
-        return await (
-            await axios.get(`http://yout-service-cotation-api.xyz`, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-        ).data
-    } catch (error) {
-        return null
-    }
 }
