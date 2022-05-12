0a16a816916b8903915a0e6c47691e2b8ab58c7d
Robson <robson.miranda@lunes.io>
2022-05-12 12:50:06 -0300

    added: open in new tab when installed

--- public/background.js
+++ public/background.js
@@ -2,5 +2,13 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Chrome extension successfully installed!")
 
-    return
+    chrome.tabs.create({
+        url: "index.html"
+        // focused: true,
+        // width: 359,
+        // height: 580,
+        // type: "popup",
+        // top: 0,
+        // left: 1500
+    })
 })
