b306c6bcbc1938426e8d446d2aad7e367a2cc3a1
Robson <robson.miranda@lunes.io>
2022-06-08 10:48:11 -0300

    fix: support button

--- src/pages/home/components/buttonsContainer.tsx
+++ src/pages/home/components/buttonsContainer.tsx
@@ -29,7 +29,6 @@ const ButtonsContainer = () => {
                 <Styles.Label>{translate.home.receive}</Styles.Label>
             </Styles.Column>
 
-
             <Styles.Column>
                 <Styles.Button
                     onClick={() => {
@@ -44,18 +43,16 @@ const ButtonsContainer = () => {
                 <Styles.Label>{translate.home.explorer}</Styles.Label>
             </Styles.Column>
 
-
             <Styles.Column>
-                <Styles.Button onClick={() => {
-                    window.open(`mailto:support@lunes.io`, "_blank")
-                }}>
+                <Styles.Button
+                    onClick={() => {
+                        window.open(`mailto:suporte@lunes.io`, "_blank")
+                    }}
+                >
                     <img src={IconSupport} alt="Send" />
                 </Styles.Button>
                 <Styles.Label>{translate.home.support}</Styles.Label>
             </Styles.Column>
-
-
-
         </Styles.ButtonsHolder>
     )
 }
